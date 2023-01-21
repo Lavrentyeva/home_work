435382869a79cc9865d5c8ddc62f6ba26a801cf0
lavrenteva@2035.university <kegfxtdf01>
2023-01-21 23:28:33 +0300

    ДЗ 2 - Создать вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from group import Group
 import unittest
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -30,20 +32,20 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
         wd.get("http://localhost/addressbook/group.php")
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit gropu creation
         wd.find_element_by_name("submit").click()
 
@@ -58,7 +60,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name = "zxz", header = "zxz",footer = "zxz")
+        self.create_group(wd, Group(name = "zxz", header = "zxz",footer = "zxz"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -67,7 +69,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name = "", header = "",footer = "")
+        self.create_group(wd, Group(name = "", header = "",footer = ""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
