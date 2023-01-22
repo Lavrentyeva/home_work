1e53d3ea906f0c10d08ef7ffab9092b3211fe8ab
Vera Lavrentyeva <listopad.vaera@mail.ru>
2023-01-22 14:21:33 +0300

    Перенос перехода к основным страницам и возврата данных параметров из тестов в описание основныз методов

--- test_add_group.py
+++ test_add_group.py
@@ -19,6 +19,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, user_name, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(user_name)
@@ -32,6 +33,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/group.php")
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -47,6 +49,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit gropu creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
@@ -56,20 +59,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name = "zxz", header = "zxz",footer = "zxz"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name = "", header = "",footer = ""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
 
