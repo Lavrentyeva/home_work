2ecf8792c1e8273441c9e4ca7be4fb94810931cf
Vera Lavrentyeva <listopad.vaera@mail.ru>
2023-05-29 10:15:46 +0300

    ДЗ №11 v.2

--- model/contact.py
+++ model/contact.py
@@ -28,7 +28,7 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and (self.firstname is None or other.firstname is None or self.firstname == other.firstname) and (self.lastname is None or other.lastname is None or self.lastname == other.lastname)
