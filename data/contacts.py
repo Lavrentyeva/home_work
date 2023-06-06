0e36d003d7ca8904cf9930e21cf0811203a247c8
Vera Lavrentyeva <listopad.vaera@mail.ru>
2023-06-06 10:55:11 +0300

    ДЗ 18/ 19

---
+++ data/contacts.py
@@ -0,0 +1,17 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname1", middlename="middlename1", lastname="lastname1",
+                    nickname="nickname1", title="title1", company="company1", address="address1",
+                    homephone="homephone1", mobilephone="mobilephone1", workphone="workphone1", fax="fax1",
+                    email="email11", email2="email12", email3="email13", homepage="homepage1",
+                    bday="1", bmonth="January", byear="1999", aday="1", amonth="January",
+                    ayear="1999", address2="address12", secondaryphone="secondaryphone1", notes="notes1"),
+    Contact(firstname="firstname2", middlename="middlename2", lastname="lastname2",
+                    nickname="nickname2", title="title2", company="company2", address="address2",
+                    homephone="homephone2", mobilephone="mobilephone2", workphone="workphone2", fax="fax2",
+                    email="email21", email2="email22", email3="email23", homepage="homepage2",
+                    bday="2", bmonth="January", byear="1998", aday="2", amonth="January",
+                    ayear="1998", address2="address22", secondaryphone="secondaryphone2", notes="notes2")
+]
